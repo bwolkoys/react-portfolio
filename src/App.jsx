466d3df0bd568b46d3017efe93ea12c08c50da91
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-11-18 10:54:41 -0800

    added code to app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Header from "../src/components/Header";
+import About from "../src/components/About";
+import Contact from "../src/components/Contact";
+import Portfolio from "../src/components/Portfolio";
+import Resume from "../src/components/Resume";
+import Footer from "../src/components/Footer";
+import "./App.css"; 
+
+function App() {
+	const [currentTab, changeTab] = useState("about");
+
+	const renderTab = () => {
+		switch (currentTab) {
+			case "about":
+				return <About />;
+			case "portfolio":
+				return <Portfolio />;
+			case "contact":
+				return <Contact />;
+			case "resume":
+				return <Resume />;
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div>
+			<div className="header">
+				<Header currentTab={currentTab} changeTab={changeTab}></Header>
+			</div>
+			<div>
+				<main>{renderTab()}</main>
+			</div>
+			<div>
+				<Footer></Footer>
+			</div>
+		</div>
+	);
+}
+
+export default App;
