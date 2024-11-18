50b840a7cf61c88ea1d0f8b6e7a3ddd966ab1075
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-11-18 11:31:14 -0800

    added code tof ooter with links to github, liinkedin, module 2 portfolio

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+        <p>
+            <ul>
+                <a href="https://github.com/bwolkoys" target="_blank" rel="noopener noreferrer">GitHub:</a>
+                <a href="https://www.linkedin.com/in/bradyn-wolkoys-b6594219a/">Linkedin:</a>
+                <a href="https://bwolkoys.github.io/Module2Challenge/">Module 2 Portfolio:</a>
+            </ul>
+        </p>
+    </footer>
+  );
+}
+
+export default Footer;
